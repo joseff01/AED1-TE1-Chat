e911a0b8959572f376a47b7d7f2c1d3165f2eb18
Jose Retana <joseantonioretanacorrales2gmail.com>
2020-10-20 15:03:00 -0600

    Cambios insignificantes

--- src/com/chat/Main.java
+++ src/com/chat/Main.java
@@ -18,9 +18,7 @@ public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
-
-        log.info("hola");
-
+        
         // Main Window creation
 
         MyWindow mainWindow = new MyWindow();
